28ccd043e3e4d563df80a4d66e85547f29bd1886
Sandeep Badeti <sandeep.badeti@compugain.com>
2020-09-24 23:39:33 +0530

    updated fedex

--- ImportExcelFIle.DotNETCore/wwwroot/cypras/cypress/integration/fedx.js
+++ ImportExcelFIle.DotNETCore/wwwroot/cypras/cypress/integration/fedx.js
@@ -1,51 +1,43 @@
-// import zipcode from '../../fixtures/zipCode.json'
+﻿// import zipcode from '../../fixtures/zipCode.json'
 // import zipCodes from '../fixtures/zipCode.json'
-import zipcode from '../fixtures/zipCode.json'
+//import zipcode from '../fixtures/zipCode.json'
 
-// clusterList = {};
 
-var zip = "65895";
 
-describe('sample', function () {
-    it('sample', async function () {
-        var count = 0;
+
         var clusterList = [];
-        // var zip = "";
-
+describe('sample', function () {
+    it('sample', function () {
         cy.visit('http://www.fedex.com/grd/maps/MapEntry.do');
-        // GetZipCode();
-        // console.log(zip);   
-    
+
+
         cy.get('input[name="originZip"]').type("10038");
         cy.get('select[name="resType"]').select('YES');
         cy.get('input[value="View overnight map"]').click();
         cy.wait(5000);
-       // console.log(zip); 
-        // cy.get('table>tbody>tr>td.small').each(($data, index, $list) => {
-           //    console.log($data, index, $list);
-        //     //clusterList.push($data.text());
-        //     //if (index == ($list.length - 1))
-        //     //    ProcessZipCluster(zipcode.zipCodes[i], clusterList);
-       //  });
-       // zip = "10310";
 
-        setInterval(() => {
-            cy.request('http://localhost:60957/Home/GetZipcode').as('getzip')
+        GetZipCode();
+    });
 
-            cy.get('@getzip').should((response) => {
-                let zip = response.body;
-                cy.get('input[name="originZip"]').clear();
-                cy.get('input[name="originZip"]').type(zip);
-                cy.get('input[value = "Update"]').click();
-                cy.wait(5000);
-                cy.get('table>tbody>tr>td.small').then(function (data) {
-                    clusterList = data.text().match(new RegExp('.{1,' + 13 + '}', 'g'));
-                    ProcessZipCluster(zip, clusterList);
-                });
-            })
-        }, 10000);
-    })
-})
+});
+ 
+
+   function GetZipCode() {
+    cy
+        .request('GET', 'https://milliardswhobackend.azurewebsites.net/api/ZipCluster/getzipcode')
+        .then((response) => {
+            let zip = response.body;
+            cy.get('input[name="originZip"]').clear();
+            cy.get('input[name="originZip"]').type(zip);
+            cy.get('input[value = "Update"]').click();
+            //cy.wait(5000);
+            cy.get('table>tbody>tr>td.small').then(function (data) {
+                clusterList = data.text().match(new RegExp('.{1,' + 13 + '}', 'g'));
+                ProcessZipCluster(zip, clusterList);
+            });
+        });
+}
+
 
 function ProcessZipCluster(zipcode, data) {
    var ZipList = [];
@@ -53,20 +45,18 @@ function ProcessZipCluster(zipcode, data) {
         let zips = cluster.split("-");
         ZipList.push({ Zipcode: zipcode, StartZip: zips[0].trim(), EndZip: zips[1].trim() })
     });
+    console.log(ZipList);
     PostToDB(ZipList);
 }
 
-// function GetZipCode() {
-//     console.log(zip);
-//     return new Promise((resolve, reject) => {
-        
-//     });
-// }
 
 function PostToDB(_data) {
     cy
-        .request('POST', 'http://localhost:60957/Home/ImportOverNightMapData', _data )
+        .request('POST', 'https://milliardswhobackend.azurewebsites.net/api/ZipCluster/updateovernightmap', _data )
         .then((response) => { 
-            console.log(response);    
+            console.log(response); 
+            GetZipCode();           
         })
 }
+
+
